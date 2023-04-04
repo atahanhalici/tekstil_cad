bc30de5518d791128a92f85d5f121b7112e903c4
atahanhalici <106783774+atahanhalici@users.noreply.github.com>
2023-04-04 05:29:05 +0300

    3D System

--- lib/pages/model_page.dart
+++ lib/pages/model_page.dart
@@ -1,5 +1,5 @@
+import 'package:babylonjs_viewer/babylonjs_viewer.dart';
 import 'package:flutter/material.dart';
-import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class ModelPage extends StatelessWidget {
   const ModelPage({Key? key}) : super(key: key);
@@ -7,10 +7,7 @@ class ModelPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Model Viewer")),
-      body: ModelViewer(
-        src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
-      ),
-    );
+        appBar: AppBar(title: const Text("Lowpoly Jacket")),
+        body: Center(child: BabylonJSViewer(src: 'assets/jacket.glb')));
   }
 }
